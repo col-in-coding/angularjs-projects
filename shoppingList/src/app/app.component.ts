dd7b697a46feaa099834ed29c9230647da21ccd0
colin <hxl.colin@gmail.com>
2017-09-28 15:19:59 -0500

    shopping list updated

--- shoppingList/src/app/app.component.ts
+++ shoppingList/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'app';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
