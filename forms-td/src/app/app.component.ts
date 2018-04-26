c50c047fbde4220f347103e649f6199f2cbcf085
colin <hxl.colin@gmail.com>
2018-04-26 10:21:29 -0400

    template driven form

--- forms-td/src/app/app.component.ts
+++ forms-td/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,7 +8,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+  	username: '',
+  	email: '',
+  	secretQuestion: '',
+  	answer: '',
+  	gender: '',
+  };
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    
+    // All the values will be reset using setValue
+    // this.signupForm.setValue({
+    // 	userData: {
+    // 		username: suggestedName,
+    // 		email: ''
+    // 	},
+    // 	secret: 'pet',
+    // 	questionAnswer: '',
+    // 	gender: 'male'
+    // });
+    
+    // To overwrite parts of the form values
+    this.signupForm.form.patchValue({
+    	userData: {
+    		username: suggestedName
+    	}
+    })
+  }
+
+  // onSubmit(form: NgForm) {
+  // 	console.log(form);
+  // }
+  
+  onSubmit() {
+  	// console.log(this.signupForm);
+  	this.submitted = true;
+  	this.user.username = this.signupForm.value.userData.username;
+  	this.user.email = this.signupForm.value.userData.email;
+  	this.user.secretQuestion = this.signupForm.value.secret;
+  	this.user.answer = this.signupForm.value.questionAnswer;
+  	this.user.gender = this.signupForm.value.gender;
+
+  	this.signupForm.reset();
   }
 }
